fbff9bd76268652c370ccc8547f75621e0a81449
vijayjoshi16 <joshivijay324@gmail.com>
2021-06-05 19:36:21 +0530

    Created initial dataframe from API data

--- app.py
+++ app.py
@@ -1,8 +1,120 @@
 from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd
+import requests
+import datetime
 
 app = Flask(__name__)
+
+
+@app.route("/getmovies", methods=["POST"])
+def get_movies():
+    movies = []
+    for i in range(1, 11):
+        link = (
+            "https://api.themoviedb.org/3/trending/all/day?api_key=7af3ff745ee1860685136e9e80d7ebce&page="
+            + str(i)
+        )
+        response = requests.get(link)
+        print(response.json())
+        results = response.json()["results"]
+        for j in range(0, len(results)):
+            movies.append(results[j])
+        print("##################")
+        print("##################")
+        print("##################")
+        print("##################")
+        print("##################")
+        print("##################")
+        print("##################")
+    print(movies)
+    cols = [
+        "title",
+        "vote_average",
+        "release_date",
+        "adult",
+        "genre_ids",
+        "vote_count",
+        "popularity",
+    ]
+    df = pd.DataFrame(columns=cols)
+
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+
+    print(df)
+
+    for movie in movies:
+        print("***********")
+        print(movie)
+        df = df.append(
+            {
+                "title": movie["name"]
+                if "title" not in movie.keys()
+                else movie["title"],
+                "vote_average": movie["vote_average"],
+                "release_date": (
+                    datetime.date.today()
+                    - datetime.datetime.strptime(
+                        movie["first_air_date"], "%Y-%m-%d"
+                    ).date()
+                ).days
+                if "first_air_date" in movie.keys()
+                else (
+                    datetime.date.today()
+                    - datetime.datetime.strptime(
+                        movie["release_date"], "%Y-%m-%d"
+                    ).date()
+                ).days,
+                "adult": 0
+                if "adult" not in movie.keys()
+                else 1
+                if movie["adult"] == True
+                else 0,
+                "genre_ids": movie["genre_ids"],
+                "vote_count": movie["vote_count"],
+                "popularity": movie["popularity"],
+            },
+            ignore_index=True,
+        )
+
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+    print("##################")
+
+    # GETTING ALL RELEVANT PARAMETERS WITHIN THE RANGE 0-10
+    # min_date = df["release_date"].min()
+    # max_date = df["release_date"].max()
+    # df["release_date"] = df["release_date"].map(
+    #     lambda release_date: ((release_date - min_date) / (max_date - min_date)) * 10
+    # )
+
+    # min_vote = df["vote_count"].min()
+    # max_vote = df["vote_count"].max()
+    # df["vote_count"] = df["vote_count"].map(
+    #     lambda vote: ((vote - min_vote) / (max_vote - min_vote)) * 10
+    # )
+
+    # min_popularity = df["popularity"].min()
+    # max_popularity = df["popularity"].max()
+    # df["popularity"] = df["popularity"].map(
+    #     lambda popularity: (
+    #         (popularity - min_popularity) / (max_popularity - min_popularity)
+    #     )
+    #     * 10
+    # )
+
+    print(df)
+    return ""
 
 
 @app.route("/getsalary", methods=["POST"])
