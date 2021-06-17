fb829ab27a0db196c42f8b0e410d3d6a4cad3af4
vijayjoshi16 <joshivijay324@gmail.com>
2021-06-17 18:44:52 +0530

    Added CORS

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import pandas as pd
 import requests
 import datetime
 from scipy.spatial import distance
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/getmovies", methods=["POST"])
